faeb9ababf626a714d9adcb48cae9ab580419577
Andy Josephson <andy.josephson@derelictmansion.com>
2020-08-26 13:59:11 -0600

    Nutrition-Tracker-App-GH-5-Added ability to save new food entries

--- src/main/java/com/derelictmansion/nutrition/controller/FoodEntryController.java
+++ src/main/java/com/derelictmansion/nutrition/controller/FoodEntryController.java
@@ -2,18 +2,23 @@ package com.derelictmansion.nutrition.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
+
 import com.derelictmansion.nutrition.data.dao.FoodEntryDao;
 import com.derelictmansion.nutrition.data.models.FoodEntry;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequestMapping(value = "/foods")
@@ -36,5 +41,11 @@ public class FoodEntryController {
   @GetMapping(produces = "application/json")
   public List<FoodEntry> getFoods(){
     return foodEntryDao.findAllFoods();
+  }
+
+  //Add food entries
+  @PostMapping(consumes = "application/json", produces = "application/json")
+  public void addFoodEntry(@Valid @RequestBody final FoodEntry foodEntry) {
+    foodEntryDao.save(foodEntry);
   }
 }
--- src/main/java/com/derelictmansion/nutrition/data/dao/FoodEntryDao.java
+++ src/main/java/com/derelictmansion/nutrition/data/dao/FoodEntryDao.java
@@ -1,8 +1,6 @@
 package com.derelictmansion.nutrition.data.dao;
 
 import com.derelictmansion.nutrition.data.models.FoodEntry;
-
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
@@ -16,8 +14,13 @@ public interface FoodEntryDao extends CrudRepository<FoodEntry, Long> {
   @Query(value = "select * from f_food", nativeQuery = true)
   public List<FoodEntry> findAllFoods();
 
+//TODO: getByName here?
+  @Query(value ="select * from f_food f where f.name =:name", nativeQuery = true)  
+  public FoodEntry getByName(String name);
+  
   public default void deleteById(Long id) {
     FoodEntry food = this.getById(id);
     save(food);
   }
+
 }
